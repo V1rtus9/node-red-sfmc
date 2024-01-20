cb628a73b0aef143709dbb74ee7db301d6fa4ab5
V1rtus9 <v.botnar@mail.ru>
2024-01-20 20:57:55 +0100

    feat: credentials

--- src/api-call.js
+++ src/api-call.js
@@ -7,11 +7,18 @@ module.exports = function (RED) {
 
 	function sfmcConnection(n) {
 		RED.nodes.createNode(this, n);
+
+		this.clientId = n.clientId;
+		this.subdomain = n.subdomain;
+		this.accountId = n.accountId;
+		this.clientSecret = n.clientSecret;
 	}
 
 	RED.nodes.registerType('sfmc', sfmcApiCall);
 	RED.nodes.registerType('sfmc_connection', sfmcConnection, {
-		api_key: { type: 'text' },
-		base_url: { type: 'text' },
+		clientId: { type: 'text' },
+		subdomain: { type: 'text' },
+		accountId: { type: 'text' },
+		clientSecret: { type: 'text' },
 	});
 };
