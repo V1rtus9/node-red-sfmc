d3e0ba4f921417ab293a8a035b8107280528bf44
V1rtus9 <v.botnar@mail.ru>
2024-01-21 11:59:34 +0100

    feat: handle request

--- src/api-call.js
+++ src/api-call.js
@@ -1,8 +1,44 @@
+const mustache = require('mustache');
+const { SfmcApi } = require('sfmc-api');
+
 module.exports = function (RED) {
 	function sfmcApiCall(config) {
 		RED.nodes.createNode(this, config);
+		const connection = RED.nodes.getNode(config.sfmc_connection);
 
-		this.on('input', async (msg) => {});
+		const apiClient = new SfmcApi({
+			clientId: connection.clientId,
+			subdomain: connection.subdomain,
+			accountId: connection.accountId,
+			clientSecret: connection.clientSecret,
+		});
+
+		this.on('input', async (msg) => {
+			const endpoint = mustache.render(config.endpoint, msg);
+
+			try {
+				const response = await (async () => {
+					switch (config.method) {
+						case 'GET':
+							return apiClient.rest.get(endpoint);
+						case 'POST':
+							return apiClient.rest.post(endpoint, msg.payload);
+						case 'PUT':
+							return apiClient.rest.put(endpoint, msg.payload);
+						case 'DELETE':
+							return apiClient.rest.delete(endpoint);
+						default:
+							throw new Error('Unsupported method');
+					}
+				})();
+
+				this.send({
+					payload: response,
+				});
+			} catch (e) {
+				this.error(e);
+			}
+		});
 	}
 
 	function sfmcConnection(n) {
